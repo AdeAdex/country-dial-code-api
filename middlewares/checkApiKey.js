976e596e480acfde076c0a5526f5825ce77d6388
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-07-23 13:22:40 +0100

    Update checkApiKey.js

--- middlewares/checkApiKey.js
+++ middlewares/checkApiKey.js
@@ -1,18 +1,26 @@
 // middlewares/checkApiKey.js
+const axios = require('axios');
 require('dotenv').config();
 
-const checkApiKey = (req, res, next) => {
+const checkApiKey = async (req, res, next) => {
   const apiKey = req.headers.authorization?.split(" ")[1];
-  
+
   if (!apiKey) {
     return res.status(401).json({ message: "API key is missing" });
   }
 
-  if (apiKey !== process.env.API_KEY) {
-    return res.status(403).json({ message: "Invalid API key" });
-  }
+  try {
+    const response = await axios.post(`${process.env.GAMEHUB_URL}/api/verify-apikey/route.js`, { apiKey });
 
-  next();
+    if (response.data.valid) {
+      next();
+    } else {
+      res.status(403).json({ message: "Invalid API key" });
+    }
+  } catch (error) {
+    console.error("Error verifying API key:", error.message);
+    res.status(500).json({ message: "Failed to verify API key" });
+  }
 };
 
 module.exports = checkApiKey;
