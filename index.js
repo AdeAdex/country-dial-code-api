b91d0a8bd78a4d7663621c159090d6e3717b2f93
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2024-10-20 07:25:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,23 @@ app.use("/api", countryRoutes);
 
 // Add a simple route for the root URL
 app.get("/", (req, res) => {
-  res.send("Welcome to the Adex; Country and Dial Code API");
+  res.status(200).json({
+    message: "Welcome to the Adex Country and Dial Code API. This API provides comprehensive data on countries and their corresponding dial codes, enabling seamless integration for applications requiring geographical and telecommunication information.",
+    documentation: "For detailed API documentation and usage guidelines, please refer to the official documentation.",
+    availableEndpoints: [
+      {
+        method: "GET",
+        path: "/api/countries",
+        description: "Fetch a complete list of countries."
+      },
+      {
+        method: "GET",
+        path: "/api/countries/:code",
+        description: "Retrieve detailed information about a specific country dial code, based on the provided country code (e.g., 'US' for the United States)."
+      }
+    ],
+    support: "For any inquiries or technical support, please contact our team at adeoluamole@gmail.com"
+  });
 });
 
 app.listen(PORT, () => {
